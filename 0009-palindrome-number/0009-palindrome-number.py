06e8a1df5ae4d4dd98004fb662bf3b3d26749f4a
JEWEL HOSSAIN <jewelhossain9091@gmail.com>
2026-07-13 15:42:44 +0530

    Time: 19 ms (8.27%), Space: 19.3 MB (53.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False 
+        
+        num = x
+        rev_sum = 0
+        while x:
+            digit = x % 10
+            rev_sum = rev_sum * 10 + digit
+            x = x // 10
+
+        return rev_sum == num
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
